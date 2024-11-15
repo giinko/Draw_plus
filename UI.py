d0b365b863d9622d5a39a2be683bb7063022b763
giinko <ginko.opro@gmail.com>
2024-11-15 16:45:25 +0100

    IDE finis

--- UI.py
+++ UI.py
@@ -10,16 +10,11 @@ class Application:
         self.root = root
         self.root.title("IDE for DRAW ++")
         self.root.geometry("1000x600")
-        
-        # Création du canvas et de l'instance Cursor
-        #self.canvas = tk.Canvas(self.root, width=800, height=400, bg="white")
-        #self.canvas.grid(row=0,column=1)
-        
-        self.menu()
+                
+        self.dossier_racine = {}
 
+        self.menu()
         self.app()
-        #self.cursor = Cursor(self.canvas_droit, x=400, y=200)
-        #self.cursor.move_forward(50)
 
 
     def menu(self):
@@ -30,7 +25,6 @@ class Application:
         #Menu fichier
         self.menu_fichier = tk.Menu(self.menu_barre, tearoff=0)
         self.menu_barre.add_cascade(label="Fichier", menu=self.menu_fichier)
-
         self.menu_fichier.add_command(label="Open folder", command=self.ouvrir_dossier)
         self.menu_fichier.add_command(label="New file",command=self.creer_fichier)
         self.menu_fichier.add_command(label="Save",command=self.enregistrer_fichier)
@@ -40,16 +34,14 @@ class Application:
         self.menu_fichier.add_command(label="Quitter", command=self.root.quit)
 
         #Menu help
-        self.menu_fichier = tk.Menu(self.menu_barre, tearoff=0)
-        self.menu_barre.add_cascade(label="Help", menu=self.menu_fichier)
+        self.menu_help = tk.Menu(self.menu_barre, tearoff=0)
+        self.menu_barre.add_cascade(label="Help", menu=self.menu_help)
 
     def app(self):
         # Cadre principal pour la disposition des widgets
         self.cadre_principal = tk.PanedWindow(self.root,orient="horizontal")
         self.cadre_principal.pack(fill="both", expand=True)
 
-
-        
         self.gestion_fichier()
         self.gestion_text()
     
@@ -62,83 +54,85 @@ class Application:
         self.frame_glob = tk.Frame(self.cadre_principal,bg="lightblue")
         self.cadre_principal.add(self.frame_glob)
 
-        self.gest = ttk.Treeview(self.frame_glob)
-        self.gest.pack(fill="both", expand=True)
+        self.treeview_window = ttk.Treeview(self.frame_glob)
+        self.treeview_window.pack(fill="both", expand=True)
 
-        self.gest.bind("<<TreeviewSelect>>", self.afficher_fichier)
+        self.treeview_window.bind("<<TreeviewSelect>>", self.afficher_fichier)
 
-        # Bouton pour ouvrir un dossier
-        self.btn_open = ttk.Button(self.frame_glob, text="Ouvrir un dossier", command=self.ouvrir_dossier)
-        self.btn_open.pack(pady=20,padx=0)
+        #Style
+        self.treeview_window.heading("#0", text="Folders", anchor="w")
 
-        # Bouton pour supp un dossier
-        self.btn_delete = ttk.Button(self.frame_glob, text="Supprimer", command=self.enregistrer_fichier)
-        self.btn_delete.pack(pady=20,padx=0)
-
-        # Configuration des colonnes et du style de l'arborescence
-        self.gest.heading("#0", text="Folders", anchor="w")
-
+    #Ouvre le menu pour selectionner le dossier a ouvrir
     def ouvrir_dossier(self):
 
-        self.folder_selected = filedialog.askdirectory()
+        folder_selected = filedialog.askdirectory()
 
-        if self.folder_selected:
-            self.root_node = self.gest.insert("", "end", text=os.path.basename(self.folder_selected), open=True)
-            self.inserer_fichier(self.folder_selected, self.root_node)
+        if folder_selected:
+            base = self.treeview_window.insert("", "end", text=os.path.basename(folder_selected), open=True)
+            self.dossier_racine[base] = folder_selected
+            self.inserer_fichier(folder_selected, base)
 
 
+
+    #Permet d'insérer un fichier dans le treeview
     def inserer_fichier(self, parent_path, parent_node):
 
         try:
             for item in os.listdir(parent_path):
-
-                self.item_path = os.path.join(parent_path, item)
-                self.node = self.gest.insert(parent_node, "end", text=item, open=False)
-
-                if os.path.isdir(self.item_path):
-                    self.inserer_fichier(self.item_path, self.node)
+                chemin_item = os.path.join(parent_path, item)
+                noeud = self.treeview_window.insert(parent_node, "end", text=item, open=False)
+                self.dossier_racine[noeud] = chemin_item
+                if os.path.isdir(chemin_item):
+                    self.inserer_fichier(chemin_item, noeud)
         except :
             pass
 
+    #Permet de retirer un dossier / fichier du treeview
+    #SUPP DANS DOSSIER RACINE NE PAS OUBLIER ! 
     def supprimer_dossier(self,dos=""):
-        self.selected_item = self.gest.focus()
-        if self.selected_item:
-            self.gest.delete(self.selected_item)
+        selected_item = self.treeview_window.focus()
+        if selected_item:
+            self.treeview_window.delete(selected_item)
 
+
+    #Permet d'afficher le fichier dans la zone de texte
     def afficher_fichier(self, event):
 
-        selected_item = self.gest.focus()  
+        selected_item = self.treeview_window.focus()  
+
         if not selected_item:
             return
-        fichier_selectionne = os.path.join(self.folder_selected, self.gest.item(selected_item, "text"))
+
+        fichier_selectionne = self.dossier_racine[selected_item]
+
         if os.path.isfile(fichier_selectionne): 
             try:
                 with open(fichier_selectionne, "r", encoding="utf-8") as f:
                     contenu = f.read()
                 self.zone_texte.delete("1.0", "end")
                 self.zone_texte.insert("1.0", contenu)
+        #Voir pour traiter les erreurs différament, mais pour le moment c'est bon
             except Exception as e:
                 self.zone_texte.delete("1.0", "end")
                 self.zone_texte.insert("1.0", f"Erreur lors de la lecture du fichier : {e}")
-        else:
-            self.zone_texte.delete("1.0", "end")
-            self.zone_texte.insert("1.0", "Sélectionnez un fichier pour afficher son contenu.")
 
 
     def enregistrer_fichier(self):
 
-        selected_item = self.gest.focus()  
+        selected_item = self.treeview_window.focus() 
+
         if not selected_item:
             messagebox.showerror("Erreur", "Aucun fichier sélectionné.")
             return
 
-        fichier_selectionne = os.path.join(self.folder_selected, self.gest.item(selected_item, "text"))
-        if os.path.isfile(fichier_selectionne):  
+        chemin_fichier = self.dossier_racine[selected_item]
+        
+        if os.path.isfile(chemin_fichier):  
             try:
                 contenu = self.zone_texte.get("1.0", "end").strip() 
-                with open(fichier_selectionne, "w", encoding="utf-8") as f:
+                with open(chemin_fichier, "w", encoding="utf-8") as f:
                     f.write(contenu)
-                messagebox.showinfo("Succès", f"Fichier enregistré : {fichier_selectionne}")
+                messagebox.showinfo("Succès", f"Fichier enregistré : {self.treeview_window.item(selected_item, "text")}")
             except Exception as e:
                 messagebox.showerror("Erreur", f"Impossible d'enregistrer le fichier : {e}")
         else:
@@ -147,48 +141,34 @@ class Application:
 
     def creer_fichier(self):
 
-        selected_item = self.gest.focus()
+        selected_item = self.treeview_window.focus()
 
         if not selected_item:
             messagebox.showerror("Erreur", "Aucun dossier sélectionné.")
             return
 
-        if os.path.isdir(self.folder_selected):  
+        chemin_dos = self.dossier_racine[selected_item]
+        if os.path.isdir(chemin_dos):  
             nom_fichier = simpledialog.askstring("Créer un fichier", "Nom du fichier (avec extension) :")
             if not nom_fichier:
                 return 
-            chemin_fichier = os.path.join(self.folder_selected, nom_fichier)
+            chemin_fichier = os.path.join(chemin_dos, nom_fichier)
             try:
                 with open(chemin_fichier, "w", encoding="utf-8") as fichier:
                     fichier.write("") 
-                    
-                #self.gest.insert(selected_item, "end", text=nom_fichier,open=False)
-                #messagebox.showinfo("Succès", f"Fichier créé : {chemin_fichier}")
+                
+
+                noeud = self.treeview_window.insert(selected_item, "end", text=nom_fichier, open=False)
+                self.dossier_racine[noeud] = chemin_fichier
+
+                messagebox.showinfo("Succès", f"Fichier créé : {nom_fichier}")
+            
             except:
                 print("erreur durant la création du fichier")
         else:
             messagebox.showerror("Erreur", "L'élément sélectionné n'est pas un dossier.")
 
-   #Faire fonction récurcive pour recup l'id de ce que je veux dans le tree 
-   #Soit on ajoute directement, soit on supp tt l'arbre et re affiche tt ?
 
-    def amelioration_plus_tard(self):
-        self.scrollbar = tk.Scrollbar(self.zone_texte)
-        self.scrollbar.pack(side="right", fill="y")
-        self.zone_texte.config(yscrollcommand=self.scrollbar.set)
-        self.scrollbar.config(command=self.zone_texte.yview)
-
-    def creer_widgets(self):
-        
-        # Boutons pour manipuler le "curseur" (Cursor)
-        self.bt1 = tk.Button(self.root, text="Avancer", command=lambda: self.cursor.move_forward(50))
-        self.bt1.pack(side="left", padx=5, pady=10)
-        
-        self.bt2 = tk.Button(self.root, text="Tourner 90°", command=lambda: self.cursor.rotate(90))
-        self.bt2.pack(side="left", padx=5, pady=10)
-        
-        self.bt3 = tk.Button(self.root, text="Dessiner un cercle", command=lambda: self.cursor.draw_circle(30))
-        self.bt3.pack(side="left", padx=5, pady=10)
 
 # Création de la fenêtre principale et lancement de l'application
 if __name__ == "__main__":
@@ -196,10 +176,3 @@ if __name__ == "__main__":
     app = Application(root)
     root.mainloop()
 
-
-"""
-a faire ensuite : 
-    - lier la zone de texte et le fichier 
-    - 
-
-"""
